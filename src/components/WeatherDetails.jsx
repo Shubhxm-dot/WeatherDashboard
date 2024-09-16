63b5e9248700e342d4dd0373435521791d2bb9cb
shubham <mr.shubhamchaudhari91@gmail.com>
2024-09-16 14:29:06 +0530

    Your commit message describing the changes

--- src/components/WeatherDetails.jsx
+++ src/components/WeatherDetails.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import './WeatherDetails.css'; // Ensure the path is correct
+
 import { getWeatherIcon } from './WeatherIcon'; // Ensure the path is correct
 
 const WeatherDetails = ({ weather }) => {
   return (
-    <div className="weather-details">
+    <div id='shubh11'className="weather-details">
       <h2>{weather.city}</h2>
       <p>{weather.temp}°C</p>
       <div className="weather-icon">
